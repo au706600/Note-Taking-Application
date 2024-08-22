b3d5d61c5cc0ef986feffd3bdd1aa869cbd0385e
au706600 <202106601@post.au.dk>
2024-08-22 20:59:58 +0200

    Added short description of the requirements of the app

--- Note-Taking Application/MainWindow.xaml.cs
+++ Note-Taking Application/MainWindow.xaml.cs
@@ -18,11 +18,20 @@ namespace Note_Taking_Application
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+   
+    /// The following features to be added as bare minimum:
+    /// 1) Add notes 
+    /// 2) Save notes (temporary or permanent) through a button of sort.
+    /// 3) Delete notes 
+    
+    /// Optional features: 
+    /// 1) Add photos
+   
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
-            InitializeComponent();
+            InitializeComponent(); 
         }
     }
 }
