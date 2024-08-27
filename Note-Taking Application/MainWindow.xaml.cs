60dc21b0e828954eef3d4ebe87fe37c56e942ad1
au706600 <202106601@post.au.dk>
2024-08-28 00:32:41 +0200

    Added some files along with the functionality of saving state before closing application.

--- Note-Taking Application/MainWindow.xaml.cs
+++ Note-Taking Application/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
@@ -20,38 +22,52 @@ namespace Note_Taking_Application
     /// Interaction logic for MainWindow.xaml
     /// </summary>
    
-    /// The following features to be added as bare minimum:
+    /// As bare minimum:
     /// 1) Add notes 
     /// 2) Save notes (temporary or permanent) through a button of sort.
-    /// 3) Delete notes 
     
     /// Optional features: 
     /// 1) Add photos
+    /// 2) A list of note sections that are basically fetched or "displayed" on the start side. 
    
     public partial class MainWindow : Window
     {
+        string file = "notes.txt";
         public MainWindow()
         {
             InitializeComponent();
-            
+
+            // Check if file exists. 
+            if (File.Exists(file))
+            {
+                // If it exists, open the textfile, read all the text file and then close it again. 
+                string noteContent = File.ReadAllText(file);
+                // The textbox text should be displayed from the file. 
+                notebox.Text = noteContent;
+            }
         }
 
 
         // Button for navigating back
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-
+            
         }
 
         // Button for saving notes. 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-
+            // Write the contents of the textbox text to the notes.txt file. If the target file already exists, 
+            // it will be overwritten. 
+            File.WriteAllText(file, notebox.Text);
+            // As a confirmation, display this message to the user. 
+            MessageBox.Show("Your notes are successfully saved :)");
         }
 
         // Textbox for writing notes
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
+            
             notebox.VerticalScrollBarVisibility = ScrollBarVisibility.Visible;
             notebox.SpellCheck.IsEnabled = true;
             // Set it as default background color
---
+++ Note-Taking Application/Window1.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Note_Taking_Application
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        // Function for displaying a list of note sections. 
+        private void Lists_of_Notes_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //this.dfdf.Background = new SolidColorBrush(Colors.Blue);
+        }
+
+        // Function for adding a section note. 
+        private void Add_btn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
